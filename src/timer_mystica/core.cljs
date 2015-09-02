8eeeb8b3b3c9418a763c070f1031acf63000674f
dphilipson <dphilipson@palantir.com>
2015-09-02 15:04:10 -0700

    Some basic layout
    We have the basic layout for the display now. The styling can certainly
    be improved and no effort has been made to have correct colors yet, but
    at least things are roughly in the right places.

--- src/timer_mystica/core.cljs
+++ src/timer_mystica/core.cljs
@@ -1,6 +1,6 @@
 (ns ^:figwheel-always timer-mystica.core
-    (:require
-              [reagent.core :as reagent :refer [atom]]))
+  (:require
+    [reagent.core :as r]))
 
 (enable-console-print!)
 
@@ -8,18 +8,75 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(def test-data
+  {:current-player {:faction "Witches" :time-used-ms 301000}
+   :active-players [{:faction "Dwarves" :time-used-ms 128000}
+                    {:faction "Nomads" :time-used-ms 777000}]
+   :passed-players [{:faction "Giants" :time-used-ms 123000}
+                    {:faction "Mermaids" :time-used-ms 345000}]})
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+(defonce app-state (r/atom test-data))
 
-(reagent/render-component [hello-world]
-                          (. js/document (getElementById "app")))
+;; Clock
+
+(defn format-time [ms]
+  (let [total-seconds (/ ms 1000)
+        minutes (quot total-seconds 60)
+        seconds (rem total-seconds 60)
+        seconds-str (if (< seconds 10) (str "0" seconds) (str seconds))]
+    (str minutes ":" seconds-str)))
+
+(defn main-clock [time-used-ms]
+  [:div.clock-area
+   [:p.clock (format-time time-used-ms)]])
+
+(defn active-faction-label [faction]
+  [:p.active-faction-label faction])
+
+;; Current player area
+
+(defn current-player-area [{:keys [faction time-used-ms]}]
+  [:div.current-player-area
+   [active-faction-label faction]
+   [main-clock time-used-ms]
+   [:div.button-area
+    [:button.pass-button "Pass"]
+    [:button.done-button "Done"]]])
+
+;; Active player area
+
+(defn player-list-item [{:keys [faction time-used-ms]}]
+  [:div.player-item
+   [:p.faction-label faction]
+   [:p.timer (format-time time-used-ms)]])
+
+(defn active-players-area [active-players]
+  [:div.active-players-area
+   [:p.player-list-label "Active:"]
+   (for [player active-players] [player-list-item player])])
+
+;; Passed player area
+
+(defn passed-players-area [passed-players]
+  [:div.passed-players-area
+   [:p.player-list-label "Passed:"]
+   (for [player passed-players] [player-list-item player])])
+
+;; Main component
+
+(defn timer-mystica [{:keys [current-player active-players passed-players]}]
+  [:div.timer-mystica
+   [current-player-area current-player]
+   [active-players-area active-players]
+   [passed-players-area passed-players]])
+
+(r/render-component [timer-mystica @app-state]
+                    (. js/document (getElementById "app")))
 
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
 
